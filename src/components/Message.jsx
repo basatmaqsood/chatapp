796d5806e5610ba8d2fc60c8d417dd2ce3051d6a
basat <itzbasatmaqsood@gmail.com>
2023-10-24 13:14:05 -0700

    2nd  commit

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-export default function Message() {
+export default function Message({owner,content}) {
   return (
-    <div className="message owner">
+    <div className={`message ${owner}`}>
       <div className="messageInfo">
         <img
           src="https://images.pexels.com/photos/1656684/pexels-photo-1656684.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
@@ -11,11 +11,11 @@ export default function Message() {
         <span>Just now</span>
       </div>
       <div className="messageContent">
-        <p>Hello</p>
-        <img
+        <p>{content?content:"hello"}</p>
+        {/* <img
           src="https://images.pexels.com/photos/1656684/pexels-photo-1656684.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt=""
-        />
+        /> */}
       </div>
     </div>
   );
--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -2,16 +2,16 @@ import Message from "./Message";
 function Messages() {
   return (
     <div className="messages">
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
+      <Message owner={"owner"} content={"hi"}/>
+      <Message content={"yes"} />
+      <Message content={"whats up?"} owner={"owner"}/>
+      <Message content={"Nothing important. Whats up with you?"}/>
+      <Message content={"Just chilling with boiz"} owner={"owner"}/>
+      <Message content={"great!"}/>
+      <Message content={"when are you going to Islamabad?"} owner={"owner"}/>
+      <Message content={"Tomorrow. You wanna join?"}/>
+      <Message content={"Yeah!"} owner={"owner"}/>
+      <Message content={"Great! lets see you tomorrow"}/>
     </div>
   );
 }
